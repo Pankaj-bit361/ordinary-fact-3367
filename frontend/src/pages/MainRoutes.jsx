1ba46be55becde2f64cbb76cf41244201d1b4dc6
rohit715131 <rohit715131@gmail.com>
2023-05-04 20:16:34 +0530

    add files

---
+++ frontend/src/pages/MainRoutes.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import DashboardMain from "../pages/Admin/DashboardMain";
+import ProductList from "../pages/Admin/ProductList";
+import AdminAddProduct from "../pages/Admin/AdminAddProduct";
+import UserList from "./Admin/UserList";
+import AdminEditProduct from "./Admin/AdminEditProduct";
+import OrderList from "./Admin/OrderList";
+
+const MainRoute = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<DashboardMain />} />
+        <Route path="/product-list" element={<ProductList />} />
+        <Route path="/add-product" element={<AdminAddProduct />}></Route>
+        <Route path="/edit-product/:id" element={<AdminEditProduct />}></Route>
+        <Route path="/user-list" element={<UserList />} />
+        <Route path="/order-list" element={<OrderList />} />
+
+        {/* <Route path="*" element={<PageNotFound />}></Route> */}
+      </Routes>
+    </div>
+  );
+};
+
+export default MainRoute;
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,26 @@
+import {
+  legacy_createStore,
+  combineReducers,
+  compose,
+  applyMiddleware,
+} from "redux";
+import { reducer as adminProductReducer } from "./AdminProductReducer/reducer";
+import { reducer as usersReducer } from "./UsersReducer/reducer";
+import { reducer as orderReducer } from "./OrderReducer/reducer";
+import { reducer as ProductReducer } from "./Product/reducer";
+
+import thunk from "redux-thunk";
+
+const root_reducer = combineReducers({
+  adminProductReducer,
+  usersReducer,
+  orderReducer,
+  ProductReducer,
+});
+
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+
+export const store = legacy_createStore(
+  root_reducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
